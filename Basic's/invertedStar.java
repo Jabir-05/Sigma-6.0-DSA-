e620b1dd874293f739aa399df509a6b6ddcad9e0
Jabir Imteyaz <144909463+Jabir-05@users.noreply.github.com>
2025-06-05 01:18:15 +0530

    Add files via upload

---
+++ Basic's/invertedStar.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class invertedStar {
+    public static void main(String[] args) {
+        int n =8;
+        for(int i =1; i <=n; i++){
+            for (int j=1 ; j<=(n-i +1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
