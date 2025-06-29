f13f8bb019d8bdaee8b7bc0b1b80e2e1410bc055
Jabir Imteyaz <144909463+Jabir-05@users.noreply.github.com>
2025-06-29 21:55:33 +0530

    Add files via upload

---
+++ Sortings/Counting.java
@@ -0,0 +1,33 @@
+public class Counting {
+    public static void CountingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for(int i =0; i <arr.length;i++){
+            largest = Math.max(largest, arr[i]);
+        }
+        int coutn [] = new  int [largest+1];
+        for(int i =0; i<arr.length; i++){
+            coutn [arr[i]]++;
+        }
+        int j =0;
+        for (int i=0; i<coutn.length;i++){
+            while (coutn [i] > 0) {
+                arr[j] =i;
+                j++;
+                coutn[i]--;
+                
+            }
+        }
+        }
+        public static void printArr(int arr[]){
+        for(int i =0; i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 1, 3, 2, 4, 3, 7 };
+        CountingSort(arr);
+        printArr(arr);
+    }
+}
