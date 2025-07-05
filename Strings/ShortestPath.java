0513ccac3209254586714627cc2f64c1bb20ca35
Jabir-05 <knock5jabir@gmail.com>
2025-07-05 15:38:03 +0530

    DSA

---
+++ Strings/ShortestPath.java
@@ -0,0 +1,34 @@
+public class ShortestPath {
+
+    public static float getShortestPath(String path) {
+        int x = 0, y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            // S dir
+            if (dir == 'S') {
+                y--;
+            }
+            // N dirc
+            else if (dir == 'N') {
+                y++;
+            }
+            // W dir
+            else if (dir == 'W') {
+                x--;
+            }
+            // E dir
+            else {
+                x++;
+            }
+        }
+        int X2 = x * x;
+        int Y2 = y * y;
+        return (float) Math.sqrt(X2 + Y2);
+
+    }
+
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
+    }
+}
