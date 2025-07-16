9443693793d6ff9b96c9d1fc80910958c709ac90
Jabir-05 <knock5jabir@gmail.com>
2025-07-16 23:37:36 +0530

    setithbit

---
+++ BitManipulation/setIthBit.java
@@ -0,0 +1,12 @@
+package BitManipulation;
+
+
+public class setIthBit {
+public static int setIthbit(int n, int i){
+    int bitMask = 1<<i;
+    return n| bitMask;
+}
+    public static void main(String[] args) {
+        System.out.println(setIthbit(10, 2));
+    }
+}
