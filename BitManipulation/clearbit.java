170844f1123b04dd3b0f349f1afa86576caec01b
Jabir-05 <knock5jabir@gmail.com>
2025-07-16 23:52:26 +0530

    Update

--- BitManipulation/clearbit.java
+++ BitManipulation/clearbit.java
@@ -5,7 +5,12 @@ public class clearbit {
         int bitMask =~(1<<i);
         return n & bitMask;
     }
+    public static int update(int n, int i,int newBit){
+        n = ClearBit(n, i);
+        int bitMask = newBit<<i;
+        return n | bitMask;
+    }
     public static void main(String[] args) {
-        System.out.println(ClearBit(10, 1));
+        System.out.println(update(10, 2, 1));
     }
 }
