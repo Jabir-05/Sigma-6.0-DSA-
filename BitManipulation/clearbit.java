4f44c1720419825cc10fdcc910d3dfa096c25fd8
Jabir-05 <knock5jabir@gmail.com>
2025-07-16 23:43:57 +0530

    clearbit

---
+++ BitManipulation/clearbit.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+
+public class clearbit {
+    public static int ClearBit(int n, int i){
+        int bitMask =~(1<<i);
+        return n & bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(ClearBit(10, 1));
+    }
+}
