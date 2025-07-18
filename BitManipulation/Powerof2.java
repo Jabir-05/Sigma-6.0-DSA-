39f7d17de2db6269e8bcc60bdd0d23d9795a5623
Jabir-05 <knock5jabir@gmail.com>
2025-07-18 23:10:40 +0530

    bitManipulation

---
+++ BitManipulation/Powerof2.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+
+public class Powerof2 {
+    public static boolean IspowerOfTwo(int n){
+        return (n & (n-1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(IspowerOfTwo(7));
+    }
+}
