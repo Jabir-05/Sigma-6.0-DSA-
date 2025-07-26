330793d4713119c69ea0d15bee4da01061fa2d7f
Jabir-05 <knock5jabir@gmail.com>
2025-07-26 14:45:35 +0530

    WithNumber

---
+++ PatternPrint/InvertedPyramidwithnumber.java
@@ -0,0 +1,15 @@
+public class InvertedPyramidwithnumber {
+    public static void WithNumber(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(j+ " ");
+
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        WithNumber(5);
+    }
+}
