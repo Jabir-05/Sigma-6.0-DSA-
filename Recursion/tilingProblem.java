ad6a86cea375ffdbf7463aba475d45d11b8f1419
Jabir-05 <knock5jabir@gmail.com>
2025-07-31 16:30:39 +0530

    RemoveDuplicates

---
+++ Recursion/tilingProblem.java
@@ -0,0 +1,19 @@
+public class tilingProblem {
+    public static int tilingProblem(int n) {
+        // base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        // vertical choice
+        int fnm1 = tilingProblem(n - 1);
+        // horizontal choice
+        int fnm2 = tilingProblem(n - 2);
+        // total ways
+        int totoalWyas = fnm1 + fnm2;
+        return totoalWyas;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tilingProblem(4));
+    }
+}
