3dd1972c2a63ca8b161ba2b598a478a02cbee64b
Jabir-05 <knock5jabir@gmail.com>
2025-08-05 00:36:33 +0530

    HashMap

---
+++ Hashing/hashmapExample.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+public class hashmapExample {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hm= new HashMap<>();
+         
+        // insert
+        hm.put("india",100);
+        hm.put("china",150);
+        hm.put("US " ,50);
+        System.out.println(hm);
+
+
+        //get
+    //     int Population = hm.get("india");
+    //     System.out.println(Population);
+    //     System.out.println(hm.get("indonesia"));
+
+    //     //containsKey 
+
+    // System.out.println(hm.containsKey("india")); // true
+    // System.out.println(hm.containsKey("Nepal")); //false
+
+
+    // remove
+    // System.out.println(hm.remove("india"));
+    // System.out.println(hm);
+
+    // size
+    System.out.println(hm.size());
+
+    // Is empty
+    hm.clear();
+    System.out.println(hm.isEmpty());
+    
+    }
+}
