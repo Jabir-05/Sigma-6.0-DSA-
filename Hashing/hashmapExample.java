a99dc7237404dc3e94f99c8d308693f61fe25be3
Jabir-05 <knock5jabir@gmail.com>
2025-08-06 00:48:56 +0530

    loop

--- Hashing/hashmapExample.java
+++ Hashing/hashmapExample.java
@@ -37,6 +37,8 @@ public class hashmapExample {
     //Iterate
     Set<String> keys = hm.keySet();
     System.out.println(keys);
-    
+    for(String k : keys ){
+        System.out.println("key ="+k+",value"+hm.get(k));
+    }
     }
 }
