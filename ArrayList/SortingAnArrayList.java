d373e8efddf831bda0536d6950c4d5eec15bdcf0
Jabir-05 <knock5jabir@gmail.com>
2025-09-02 19:42:10 +0530

    MultiDiamentional

---
+++ ArrayList/SortingAnArrayList.java
@@ -0,0 +1,25 @@
+// import java.util.ArrayList;
+// import java.util.Collection;
+// import java.util.Collections;
+import java.util.*;
+
+public class SortingAnArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+
+        System.out.println(list);
+        Collections.sort(list); // asecending
+        System.out.println(list);
+
+
+        // Descending
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println(list);
+
+    }
+}
